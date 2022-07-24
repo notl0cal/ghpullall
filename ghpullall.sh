eb6c1810137972f17a6752a55c9be1c8d0219825
chance-priv2 <forchancee@gmail.com>
2022-07-24 01:35:59 -0500

    Fixed File Check @l10 for copying files to

--- ghpullall.sh
+++ ghpullall.sh
@@ -4,13 +4,13 @@ for i in `ls "$REPOSTASH/"`; do
 	if [ -d "$REPOSTASH/$i" ]; then
 		if [ -d "$REPOSTASH/$i/.git" ]; then
 			cd "$REPOSTASH/$i"; pwd
-			git fetch; git status; git pull; echo "Pulling..."
+			git fetch; git status; echo "Pulling..."; git pull
 			sleep 0.5
-			cp $REPOSTASH/$i/*.sh $SHSTASH/$i  2> /dev/null; echo "Copied..."
-            if [ -d "$SHSTASH/$i.sh" ]; then
-                echo "Done."
+			cp $REPOSTASH/$i/*.sh $SHSTASH/$i  2> /dev/null
+           	if [ -f "/home/chance/bin/$i" ]; then
+                echo "Stashed.."
             fi
-	    fi
+	   	fi
 	fi
 done
 sleep 0.3
