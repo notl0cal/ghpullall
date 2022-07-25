c2a9abbe41c6aec9038d8fcf08915d4108416746
notl0cal <forchancee@gmail.com>
2022-07-25 09:45:24 -0500

    Added stashed file count to end of script output

--- ghpullall.sh
+++ ghpullall.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-source $HOME/ghpullall/config.sh
+source $HOME/archives/ghpullall/config.sh
+p=0
 for i in `ls "$REPOSTASH/"`; do
 	if [ -d "$REPOSTASH/$i" ]; then
 		if [ -d "$REPOSTASH/$i/.git" ]; then
 			cd "$REPOSTASH/$i"; pwd
 			git fetch; git status; echo "Pulling..."; git pull
 			sleep 0.5
-			cp "$REPOSTASH/$i/"*.sh "$SHSTASH/$i" 2> /dev/null
-           	if [ -f "/home/chance/bin/$i" ]; then
+			cp "$REPOSTASH/$i/"*.sh "$SHSTASH/$i" 2> /dev/null; ((p=p+1))
+           	if [ -f "$SHSTASH/$i" ]; then
                 echo "Stashed.."
             fi
 	   	fi
 	fi
 done
+echo "Files Stashed: $p"
 sleep 0.3
